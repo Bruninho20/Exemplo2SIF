027a3ea61371a292c5052abb4f4f2f32a4b4972e
Bruninho20 <99261881+Bruninho20@users.noreply.github.com>
2022-03-28 11:46:01 -0300

    Add files via upload

---
+++ Paciente.java
@@ -0,0 +1,14 @@
+package Principal;
+import Paciente.Paciente;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Paciente p = new Paciente();
+		p.setNome("Brunão do Fiesta");
+		System.out.println(p.getNome());
+		
+	}
+
+}
